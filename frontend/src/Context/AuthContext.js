5d2909bb68903b5b4d6812c2fd999c77cd5015fe
Juan Guzman (JG)² <juanjguzman15@gmail.com>
2025-09-29 20:01:14 -0500

    Use API_URL for authentication endpoint

--- frontend/src/Context/AuthContext.js
+++ frontend/src/Context/AuthContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from "react";
 
 export const AuthContext = createContext();
-
+export const API_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [token, setToken] = useState(null);
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }) => {
     const isAuthenticated = !!user;
 
     const login = async (email, password) => {
-        const res = await fetch("http://localhost:3001/api/auth/ingreso",{
+        const res = await fetch(`${API_URL}/api/auth/ingreso`,{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
